17f87a272e62f2d4f479c177e8498bf6b8be4caa
canioves <dark.gary@yandex.ru>
2024-08-20 20:28:17 +0300

    add .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,19 +8,27 @@ import { User } from './users/users.model';
 import { Board } from './boards/boards.model';
 import { Card } from './cards/cards.model';
 import { Comment } from './comments/comments.model';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '0000',
-      database: 'trello_mock',
-      models: [User, Board, Card, Comment],
-      retryAttempts: 1,
-      synchronize: true,
-      autoLoadModels: true,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: Number(configService.get('DB_PORT')),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        models: [User, Board, Card, Comment],
+        retryAttempts: 1,
+        synchronize: true,
+        autoLoadModels: true,
+      }),
     }),
     UsersModule,
     BoardsModule,
@@ -28,6 +36,6 @@ import { Comment } from './comments/comments.model';
     CommentsModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [ConfigService],
 })
 export class AppModule {}
